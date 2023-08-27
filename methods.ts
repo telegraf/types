d10561adf4df6c170755612cff489283a6735c4a
Muthu Kumar <muthukumar@thefeathers.in>
2023-08-27 22:50:25 +0530

    fix: silly args mistake with unpinAllGeneralForumTopicMessages

--- methods.ts
+++ methods.ts
@@ -1103,10 +1103,10 @@ export type ApiMethods<F> = {
   }): true;
 
   /** Use this method to clear the list of pinned messages in a General forum topic. The bot must be an administrator in the chat for this to work and must have the can_pin_messages administrator right in the supergroup. Returns True on success. */
-  unpinAllGeneralForumTopicMessages(
+  unpinAllGeneralForumTopicMessages(args: {
     /** Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername) */
-    chat_id: number | string,
-  ): true;
+    chat_id: number | string;
+  }): true;
 
   /** Use this method to send answers to callback queries sent from inline keyboards. The answer will be displayed to the user as a notification at the top of the chat screen or as an alert. On success, True is returned.
 
