e8d34c7eaac433a63e1b840136c87e161e8f2821
Daniel Rhodes <rhodes.daniel@gmail.com>
2024-08-01 16:03:40 -0700

    Add support for 7.8

--- methods.ts
+++ methods.ts
@@ -1070,6 +1070,8 @@ export type ApiMethods<F> = {
 
   /** Use this method to add a message to the list of pinned messages in a chat. If the chat is not a private chat, the bot must be an administrator in the chat for this to work and must have the 'can_pin_messages' admin right in a supergroup or 'can_edit_messages' admin right in a channel. Returns True on success. */
   pinChatMessage(args: {
+    /** Unique identifier of the business connection on behalf of which the message will be pinned */
+    business_connection_id?: string;
     /** Unique identifier for the target chat or username of the target channel (in the format `@channelusername`) */
     chat_id: number | string;
     /** Identifier of a message to pin */
@@ -1080,6 +1082,8 @@ export type ApiMethods<F> = {
 
   /** Use this method to remove a message from the list of pinned messages in a chat. If the chat is not a private chat, the bot must be an administrator in the chat for this to work and must have the 'can_pin_messages' admin right in a supergroup or 'can_edit_messages' admin right in a channel. Returns True on success. */
   unpinChatMessage(args: {
+    /** Unique identifier of the business connection on behalf of which the message will be unpinned */
+    business_connection_id?: string;
     /** Unique identifier for the target chat or username of the target channel (in the format `@channelusername`) */
     chat_id: number | string;
     /** Identifier of a message to unpin. If not specified, the most recent pinned message (by sending date) will be unpinned. */
